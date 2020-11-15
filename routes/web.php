b0b731e07b04b4668c2800ca3055bcd986e4021d
Lazarevvvv <vladlazarev1998@gmail.com>
2020-11-15 22:39:58 +0200

    Product Category Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix' => 'admin', 'namespace' => '\App\Http\Controllers\Admin', 'middleware' => 'auth'], function() {
+    Route::get('/', [HomeController::class, 'index']);
+    Route::resource('/product', ProductController::class);
+    Route::resource('/category', CategoryController::class);
 });
 
 Auth::routes();
