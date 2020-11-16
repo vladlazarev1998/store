589ad4213acc14a7759669a6e035ce0cd50e7497
Lazarevvvv <vladlazarev1998@gmail.com>
2020-11-16 21:11:56 +0200

    Database Migrations

--- database/migrations/2020_11_15_202644_create_products_table.php
+++ database/migrations/2020_11_15_202644_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('slug')->unique();
+            $table->float('price')->default('0.00');
             $table->timestamps();
         });
     }
--- database/migrations/2020_11_15_204110_create_config_table.php
+++ database/migrations/2020_11_15_204110_create_config_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateConfigTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('config', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->string('slug')->unique();
-            $table->integer('parent_id');
+            $table->string('alias');
+            $table->string('value');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('config');
     }
 }
